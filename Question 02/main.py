b459031c44a4dcb900d80894d665bbc669e496ad
Madhushika123 <96645@fhss.sjp.ac.lk>
2021-06-18 03:31:21 -0700

    push and disply linked list

---
+++ Question 02/main.py
@@ -0,0 +1,8 @@
+from stack import Stack
+
+mystack = Stack()
+print(mystack.isempty())
+mystack.push(100)
+mystack.push(200)
+mystack.push(300)
+mystack.disply()
--- Question 02/stack.py
+++ Question 02/stack.py
@@ -8,3 +8,27 @@ class Stack:
     # Constructor Stack
     def __init__(self):
         self.head = None
+
+    def isempty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def push(self, data):
+        if self.head == None:
+            self.head = Node(data)
+        else:
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
+
+    def disply(self):
+        iternode = self.head
+        if self.isempty():
+            print("Stack Umderflow")
+        else:
+            while(iternode != None):
+                print(iternode.data,  end=' ==> ')
+                iternode = iternode.next
+            return

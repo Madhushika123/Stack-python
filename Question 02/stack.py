bfe1a5b5954a36f8b07f198e3696a1c7ae182fb2
Madhushika123 <96645@fhss.sjp.ac.lk>
2021-06-18 02:09:17 -0700

    Constructor Stack

--- Question 02/stack.py
+++ Question 02/stack.py
@@ -3,3 +3,8 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class Stack:
+    # Constructor Stack
+    def __init__(self):
+        self.head = None

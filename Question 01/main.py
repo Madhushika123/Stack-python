7e47a606334aede6f5a72d4e366a7dd97cb77a1b
Madhushika123 <96645@fhss.sjp.ac.lk>
2021-06-18 02:50:05 -0700

    print stack

--- Question 01/main.py
+++ Question 01/main.py
@@ -1,3 +1,9 @@
 from stack import Stack
 
 mystack = Stack()
+print(mystack.isEmptyStack())
+print(mystack.push(150))
+print(mystack.push(250))
+print(mystack.push(350))
+print(mystack.push(450))
+print(mystack.top())

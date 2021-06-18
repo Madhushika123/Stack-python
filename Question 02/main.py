f0c3c00e04983dc562405958894fa6932c91bdc8
Madhushika123 <96645@fhss.sjp.ac.lk>
2021-06-18 03:42:49 -0700

    Pop(delete) data in linked list

--- Question 02/main.py
+++ Question 02/main.py
@@ -5,4 +5,12 @@ print(mystack.isempty())
 mystack.push(100)
 mystack.push(200)
 mystack.push(300)
+mystack.push(400)
+mystack.push(500)
 mystack.disply()
+
+print("\n======================================================================")
+
+mystack.pop()
+mystack.disply()
+mystack.pop()
--- Question 02/stack.py
+++ Question 02/stack.py
@@ -9,12 +9,14 @@ class Stack:
     def __init__(self):
         self.head = None
 
+    # To check is empty the linked list
     def isempty(self):
         if self.head == None:
             return True
         else:
             return False
 
+    # To push data to the linked list
     def push(self, data):
         if self.head == None:
             self.head = Node(data)
@@ -23,12 +25,23 @@ class Stack:
             newnode.next = self.head
             self.head = newnode
 
-    def disply(self):
-        iternode = self.head
+    # pop (delete) to the data in linked list
+    def pop(self):
         if self.isempty():
-            print("Stack Umderflow")
+            return None
         else:
-            while(iternode != None):
-                print(iternode.data,  end=' ==> ')
-                iternode = iternode.next
+            popped = self.head
+            self.head = self.head.next
+            popped.next = None
+            return popped.next
+
+    # To disply the linked list
+    def disply(self):
+        currentnode = self.head
+        if self.isempty():
+            print("Stack Underflow")
+        else:
+            while(currentnode != None):
+                print(currentnode.data,  end=' ==> ')
+                currentnode = currentnode.next
             return

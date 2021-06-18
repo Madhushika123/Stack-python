50d577bff3af5ff271c641ec41bf90d54d9bcc6a
Madhushika123 <96645@fhss.sjp.ac.lk>
2021-06-18 02:55:55 -0700

    pop data

--- Question 01/main.py
+++ Question 01/main.py
@@ -2,8 +2,11 @@ from stack import Stack
 
 mystack = Stack()
 print(mystack.isEmptyStack())
+print(mystack.top())
 print(mystack.push(150))
 print(mystack.push(250))
 print(mystack.push(350))
 print(mystack.push(450))
 print(mystack.top())
+print(mystack.isFullStack())
+print(mystack.pop())
